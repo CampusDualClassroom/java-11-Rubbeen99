4e1c91d2fde85bdd608e231818c0c590354d4f8c
rubbeen99 <ruben20042000@gmail.com>
2024-11-12 09:46:23 +0100

    Ejercicio_11 resuelto.

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -4,8 +4,32 @@ public class Exercise11 {
     public static void main(String[] args) {
         TVRemote redRemote = new TVRemote("Rojo");
         TVRemote blackRemote = new TVRemote("Negro");
+
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
-    }
 
+        redRemote.turnOn();
+        blackRemote.turnOn();
+
+        redRemote.channelUp();
+        blackRemote.channelDown();
+
+        redRemote.channel = 0;
+        blackRemote.channel = 0;
+
+        redRemote.channelDown();
+        blackRemote.channelDown();
+
+        redRemote.volumeUp();
+        blackRemote.volumeDown();
+
+        redRemote.volume = 0;
+        blackRemote.volume = 0;
+
+        redRemote.volumeDown();
+        blackRemote.volumeDown();
+
+        redRemote.turnOff();
+        blackRemote.turnOff();
+    }
 }
